9e3cb22fb790ba63c5feccd6e36782b4104c7050
wangbaowei <wangbaowei@coding.net>
2021-04-26 11:11:05 +0800

    zap sentry config

---
+++ zap.go
@@ -0,0 +1,178 @@
+package log
+
+import (
+	"github.com/TheZeroSlave/zapsentry"
+	"github.com/caarlos0/env/v6"
+	"github.com/cockroachdb/errors"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var (
+	Logger    *zap.Logger
+	sugar     *zap.SugaredLogger
+	SentryCfg SentryConfiguration
+)
+
+type SentryConfiguration struct {
+	Dsn string `env:"SENTRY_DSN"`
+}
+
+func init() {
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.InfoLevel)
+	Logger = zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1), zap.AddStacktrace(zapcore.WarnLevel))
+
+	// sentry attach
+	if err := env.Parse(&SentryCfg); err != nil {
+		err := errors.Wrap(err, "parse sentry configuration error")
+		Fatal("", zap.Error(err))
+	}
+	Logger = modifyToSentryLogger(Logger, SentryCfg.Dsn)
+
+	sugar = Logger.Sugar()
+}
+
+func Sync() {
+	if Logger != nil {
+		_ = Logger.Sync()
+	}
+	if sugar != nil {
+		_ = sugar.Sync()
+	}
+}
+
+func getLogWriter() zapcore.WriteSyncer {
+	return zapcore.AddSync(os.Stdout)
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.9999")
+	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func modifyToSentryLogger(logger *zap.Logger, dsn string) *zap.Logger {
+	if dsn == "" {
+		return logger
+	}
+	cfg := zapsentry.Configuration{
+		Level: zapcore.ErrorLevel,
+	}
+	core, err := zapsentry.NewCore(cfg, zapsentry.NewSentryClientFromDSN(dsn))
+	if err != nil {
+		Error("Failed to init zapsentry", zap.Error(err))
+	}
+	return zapsentry.AttachCoreToLogger(core, logger)
+}
+
+func Debug(msg string, fields ...zapcore.Field) {
+	Logger.Debug(msg, fields...)
+}
+
+func Debugf(template string, args ...interface{}) {
+	sugar.Debugf(template, args...)
+}
+
+func Debugp(args ...interface{}) {
+	sugar.Debug(args...)
+}
+
+func Debugw(msg string, keysAndValues ...interface{}) {
+	sugar.Debugw(msg, keysAndValues)
+}
+
+func Info(msg string, fields ...zapcore.Field) {
+	Logger.Info(msg, fields...)
+}
+
+func Infof(template string, args ...interface{}) {
+	sugar.Infof(template, args...)
+}
+
+func Infop(args ...interface{}) {
+	sugar.Info(args...)
+}
+
+func Infow(msg string, keysAndValues ...interface{}) {
+	sugar.Infow(msg, keysAndValues...)
+}
+
+func Warn(msg string, fields ...zapcore.Field) {
+	Logger.Warn(msg, fields...)
+}
+
+func Warnf(template string, args ...interface{}) {
+	sugar.Warnf(template, args...)
+}
+
+func Warnp(args ...interface{}) {
+	sugar.Warn(args...)
+}
+
+func Warnw(msg string, keysAndValues ...interface{}) {
+	sugar.Warnw(msg, keysAndValues...)
+}
+
+func Error(msg string, fields ...zapcore.Field) {
+	Logger.Error(msg, fields...)
+}
+
+func Errorf(template string, args ...interface{}) {
+	sugar.Errorf(template, args...)
+}
+
+func Errorp(args ...interface{}) {
+	sugar.Error(args...)
+}
+
+func Errorw(msg string, keysAndValues ...interface{}) {
+	sugar.Errorw(msg, keysAndValues...)
+}
+
+func Fatal(msg string, fields ...zapcore.Field) {
+	Logger.Fatal(msg, fields...)
+}
+
+func Fatalf(template string, args ...interface{}) {
+	sugar.Fatalf(template, args...)
+}
+
+func Fatalp(args ...interface{}) {
+	sugar.Fatal(args...)
+}
+
+func Fatalw(msg string, keysAndValues ...interface{}) {
+	sugar.Fatalw(msg, keysAndValues...)
+}
+
+func Panic(msg string, fields ...zapcore.Field) {
+	Logger.Panic(msg, fields...)
+}
+
+func Panicf(template string, args ...interface{}) {
+	sugar.Panicf(template, args...)
+}
+
+func Panicp(args ...interface{}) {
+	sugar.Panic(args...)
+}
+
+func Panicw(msg string, keysAndValues ...interface{}) {
+	sugar.Panicw(msg, keysAndValues...)
+}
+
+func DPanic(msg string, fields ...zapcore.Field) {
+	Logger.DPanic(msg, fields...)
+}
+
+func With(fields ...zapcore.Field) *zap.Logger {
+	return Logger.With(fields...)
+}
+
+func Named(s string) *zap.Logger {
+	return Logger.Named(s)
+}
